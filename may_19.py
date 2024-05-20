be709ab3826818f08af87fe1ec307c60216cbebf
prarthana-maheta <142563768+prarthana-maheta@users.noreply.github.com>
2024-05-20 21:04:31 +0530

    Add files via upload

---
+++ may_19.py
@@ -0,0 +1,19 @@
+# str1="qwertyuiopasdfghjklzxcvbnm1234567890!@#$%^&*()"
+# print(str1[5:20:2], str1[20:40:2])
+
+# split()
+str1="i am a royal am stamudent"
+
+# print(str1.split(' ',))
+
+
+# # join()
+# # str2="1234567890"
+# # print('-----'.join(str1.split(' ',)))
+# print(''.join(str1.split(" ")))
+print('***'.join(str1.partition("am")))
+# print(str1.rpartition("am"))
+
+
+# print(str1.replace("am","M"))
+
---
+++ may_20.py
@@ -0,0 +1,52 @@
+# str1 = "royal He,llo"
+
+# print(str1.capitalize())
+# print(str1.lower())
+# print(str1.upper())
+# print(str1.swapcase())
+# print(str1.title())
+# print(str1.split(" "))
+# print(' '.join(str1.split("h")))
+# #1 2 3
+#royal hello
+
+# string immutable, ordered
+
+# list = array 
+# mutable 
+# no fix size 
+# different data storing
+# string ="",''
+# list =[]
+
+# int1=123
+# str1="1234567890"
+# list1 = [123,'123',123.0,1j+1,[1,2,3,6,[6,7,8]]]
+# ab=list1[-1][-1][1]
+# print(list(int1))
+# print(list(str1))
+# print(list1[-3:-1:-1])
+# if negative direction then start should be greater then end 
+# # print(list1[2:4:1])
+# if negative direction then start should be smaller then end 
+# list12= ['qwe','rtyui',dfghj']
+
+
+list1=[100,10,1000,10000,0]
+# list1[0]=123
+# list1[1]=456
+# print(list1) cannot use this
+
+list1=['abc',123,'a67','aca','aca']
+
+# functions
+# print(sum(list1))
+print(max(list1))
+print(min(list1))
+# print(sorted(list1, reverse=True))
+
+# # reverse techniques
+# print(list1[::-1])
+# print(list(reversed(list1)))
+# list1.reverse()
+# print(list1)
